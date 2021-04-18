4d7c14e6b0255211166475fa7592c946faadea3d
Gerardo <sherarmdp@gmail.com>
2021-04-18 18:03:28 -0300

    Adding unit tests

---
+++ exercise_1/test_filter_json_exercise_1.py
@@ -0,0 +1,24 @@
+import unittest
+from exercise_1 import filter_json
+from test_consts_exercise_1 import string_json_1, \
+                        list_of_keys_1, \
+                        expected_result_1,\
+                        string_json_2,\
+                        list_of_keys_2,\
+                        expected_result_2,\
+                        string_json_3,\
+                        list_of_keys_3,\
+                        expected_result_3
+
+
+class TestFilterJson(unittest.TestCase):
+
+    def test_filter_json(self):
+        result_1 = filter_json(string_json_1, list_of_keys_1)
+        self.assertEqual(result_1, expected_result_1)
+        result_2 = filter_json(string_json_2, list_of_keys_2)
+        self.assertEqual(result_2, expected_result_2)
+        result_3 = filter_json(string_json_3, list_of_keys_3)
+        self.assertEqual(result_3, expected_result_3)
+
+
---
+++ exercise_3/test_consts_exercise_3.py
@@ -0,0 +1,75 @@
+# THIS IS A JSON FILE WITH A LIST OF OBJECTS
+string_json_1 = '''
+[
+	{
+		"id": "0001",
+		"type": "donut",
+		"name": "Cake",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" },
+						{ "id": "1003", "type": "Blueberry" },
+						{ "id": "1004", "type": "Devil's Food" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5007", "type": "Powdered Sugar" },
+				{ "id": "5006", "type": "Chocolate with Sprinkles" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0002",
+		"type": "donut",
+		"name": "Raised",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0003",
+		"type": "donut",
+		"name": "Old Fashioned",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	}
+]'''
+# SUPPOSE I WANT TO GET batters.batter[0].type of the FIRST element of the list, I could do it like this:
+list_of_keys_1 = ["[1].batters.batter[0].type"]
+expected_result_1 =
